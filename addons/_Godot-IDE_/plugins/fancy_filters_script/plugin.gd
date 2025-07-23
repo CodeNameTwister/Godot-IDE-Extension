f3e4a7b00ee3b00d356832698f4f302aec08510b
H0m3 <aid.me.service@gmail.com>
2025-07-23 13:47:34 -0400

    Change Default Switch Script Filter/List by Ctrl+G

--- addons/_Godot-IDE_/plugins/fancy_filters_script/plugin.gd
+++ addons/_Godot-IDE_/plugins/fancy_filters_script/plugin.gd
@@ -39,7 +39,7 @@ func _init() -> void:
 		_c_input_switch_panels = InputEventKey.new()
 		_c_input_switch_panels.pressed = true
 		_c_input_switch_panels.ctrl_pressed = true
-		_c_input_switch_panels.keycode = KEY_F
+		_c_input_switch_panels.keycode = KEY_G
 		IDE.set_config("fancy_filters_script", "switch_panels", _c_input_switch_panels)
 
 func _get_traduce(msg : String) -> String:
